d63c91544cc29c4afd29bacb4b5c425e08e91f1f
cristian <ext.cristian.guedes@farfetch.com>
2023-05-06 17:16:47 -0300

    fix:server-port

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import dotenv from "dotenv";
 import { setUpRoutes } from "./shared/config/set-up-routes";
 dotenv.config()
 
+
 const server = Express();
 setUpRoutes(server)
 
 server.use(Express.urlencoded({ extended: true }));
 server.use(Express.json());
 
-server.listen(process.env.SERVER_PORT,()=>console.log("server rodando na porta "+process.env.SERVER_PORT))
+server.listen(process.env.PORT||process.env.SERVER_PORT,()=>console.log("server rodando"))
