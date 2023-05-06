0bcfc47768b0a3aaa566e07cbde8e64080fa0fd2
cristian <ext.cristian.guedes@farfetch.com>
2023-05-06 19:39:40 -0300

    feat:add-routes

--- src/modules/car-registration/src/infra/database/database.ts
+++ src/modules/car-registration/src/infra/database/database.ts
@@ -1,34 +1,48 @@
+import { prisma } from "../../../../../shared/infra/db/prisma"
+import { CarRegistration } from "../../domain/entities"
+
+
 import { Entity } from "../../main/shared/IBaseEntity"
 
-type Record=Entity<any,any>
+type Record=Entity<CarRegistration,string>
 
 class Database {
-    private records:Array<Record>=[]
 
-    async create(params:Partial<Record>): Promise<Record> {
-
-        this.records.push(params)
-
-        return this.records[0]
+    async create(params:Record): Promise<Record> {
+        console.log(params)
+        return  await prisma.address.create({data:params})
 }
     
    async read(): Promise<Array<Record>> {
-            return this.records
+    console.log('read')
+
+           const address = await prisma.address.findMany()
+           if(address)return address
+            throw new Error('sem endereços');
     }
 
     public async readById(params: Pick<Record,'id'>):Promise<Record>{
-        console.log(params)
-        return this.records[0]
+    const address =await prisma.address.findUnique({ where:{id:params.id}})
+     if(address)return address
+     throw new Error('sem endereços');
+        
+        
        
     }
 
     public async update(params:Partial<Record>){
         console.log(params)
-        return this.records[0]
+        const {city,id,plate,state}=params
+        
+        return await prisma.address.update({data:{city,plate,state},where:{id}})
     }
 
-    public async delete(id:Pick<Record,'id'>){
-        console.log(id)
+    public async delete(params:Pick<Record,'id'>){
+        console.log(params)
+        
+            await prisma.address.delete({where:{id:params.id}})
+        
+        
     }
 
 
--- src/modules/part/src/infra/database/database.ts
+++ src/modules/part/src/infra/database/database.ts
@@ -1,34 +1,47 @@
+import { prisma } from "../../../../../shared/infra/db/prisma"
+import { PartDTO } from "../../../../part/src/data/Dtos/part"
+
 import { Entity } from "../../main/shared/IBaseEntity"
 
-type Record=Entity<any,any>
+type Record=Entity<PartDTO,string>
 
 class Database {
-    private records:Array<Record>=[]
 
-    async create(params:Partial<Record>): Promise<Record> {
-
-        this.records.push(params)
-
-        return this.records[0]
+    async create(params:Record): Promise<Record> {
+        console.log(params)
+        return  await prisma.address.create({data:params})
 }
     
    async read(): Promise<Array<Record>> {
-            return this.records
+    console.log('read')
+
+           const address = await prisma.address.findMany()
+           if(address)return address
+            throw new Error('sem endereços');
     }
 
     public async readById(params: Pick<Record,'id'>):Promise<Record>{
-        console.log(params)
-        return this.records[0]
+    const address =await prisma.address.findUnique({ where:{id:params.id}})
+     if(address)return address
+     throw new Error('sem endereços');
+        
+        
        
     }
 
     public async update(params:Partial<Record>){
         console.log(params)
-        return this.records[0]
+        const {email,involvement,name,id}=params
+        
+        return await prisma.address.update({data:{email,involvement,name},where:{id}})
     }
 
-    public async delete(id:Pick<Record,'id'>){
-        console.log(id)
+    public async delete(params:Pick<Record,'id'>){
+        console.log(params)
+        
+            await prisma.address.delete({where:{id:params.id}})
+        
+        
     }
 
 
